a890ccbcec4a8ab95dd896b9fdcd2bee4d5a01b5
sHyunis <162306191+sHyunis@users.noreply.github.com>
2026-03-21 19:01:24 +0900

    feat: RoomBookingPage용 getRoomsQueryOptions 추가

---
+++ src/pages/RoomBookingPage/hooks/getRoomsQueryOptions.ts
@@ -0,0 +1,10 @@
+import { queryOptions } from '@tanstack/react-query';
+import { getRooms } from 'pages/remotes';
+
+export function getRoomsQueryOptions() {
+  return queryOptions({
+    queryKey: ['rooms'] as const,
+    queryFn: () => getRooms(),
+    staleTime: 1000 * 60 * 5,
+  });
+}
