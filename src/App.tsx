4008c5ca6007716b11d8418996b305f7e722cdd5
sHyunis <162306191+sHyunis@users.noreply.github.com>
2026-03-21 19:00:11 +0900

    feat: QueryClientProvider, ToastProvider 설정

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { css, Global } from '@emotion/react';
 import { useState } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { GlobalPortal } from './GlobalPortal';
+import { DialogProvider } from 'shared/components/Dialog';
+import { ToastProvider } from 'shared/components/Toast';
 
 import '_tosslib/sass/app.scss';
 import { PageLayout } from 'pages/PageLayout';
@@ -23,21 +25,25 @@ export default function App() {
 
   return (
     <QueryClientProvider client={queryClient}>
-      <GlobalPortal.Provider>
-        <Global
-          styles={css`
-            ${normalize}
-            h1, h2, h3, h4, h5, h6 {
-              font-size: 1em;
-              font-weight: normal;
-              margin: 0; /* or '0 0 1em' if you're so inclined */
-            }
-          `}
-        />
-        <PageLayout>
-          <Routes />
-        </PageLayout>
-      </GlobalPortal.Provider>
+      <ToastProvider>
+        <DialogProvider>
+          <GlobalPortal.Provider>
+          <Global
+            styles={css`
+              ${normalize}
+              h1, h2, h3, h4, h5, h6 {
+                font-size: 1em;
+                font-weight: normal;
+                margin: 0; /* or '0 0 1em' if you're so inclined */
+              }
+            `}
+          />
+          <PageLayout>
+            <Routes />
+          </PageLayout>
+        </GlobalPortal.Provider>
+        </DialogProvider>
+      </ToastProvider>
     </QueryClientProvider>
   );
 }
